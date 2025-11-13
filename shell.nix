54d6a68bdb4e1cdaaf5b686457ec1ba9938580d9
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-11-13 15:08:51 -0700

    Update shell.nix to match flake.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.apitrace
-    pkgs.cmake
-    pkgs.gcc
-    pkgs.glbinding # For C++ bindings
-    pkgs.glew
-    pkgs.glfw2
-    pkgs.libGLU
-    pkgs.mesa
+let
+  # If you want the same pkgs config as the flake (unfree & CUDA), you would
+  # need to import the same pinned channel or use flakes. Here we keep default.
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    cmake
+    ninja
+    gcc
   ];
+
+  buildInputs = with pkgs; [
+    libGL
+    libGLU
+    glfw
+    # linuxPackages.nvidia_x11  # kernel-specific; include only if you truly need it
+  ];
+
+  # Make sure driver libraries are found at runtime when running inside the shell
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (nativeBuildInputs ++ buildInputs);
 }
