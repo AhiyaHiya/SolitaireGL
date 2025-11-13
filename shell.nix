1fd61b2fe16d544fcc55c36fa235dea07fa64de3
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-11-13 12:46:35 -0700

    Sort libraries in shell.nix

--- shell.nix
+++ shell.nix
@@ -2,13 +2,13 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.gcc
-    pkgs.cmake
-    pkgs.libGLU
-    pkgs.glew
-    pkgs.glbinding # For C++ bindings
-    pkgs.glfw2
-    pkgs.mesa
     pkgs.apitrace
+    pkgs.cmake
+    pkgs.gcc
+    pkgs.glbinding # For C++ bindings
+    pkgs.glew
+    pkgs.glfw2
+    pkgs.libGLU
+    pkgs.mesa
   ];
 }
